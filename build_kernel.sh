d63155bdc89bd3ab6a43b6240ea1fe17591c4460
Peter-McAlister-s3294034 <s3294034@student.rmit.edu.au>
2020-11-20 09:56:41 +1100

    build kernel 20201119 01

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,4 +5,9 @@
 ********** Assignment: 2 ********************************
 *********************************************************'
 
-
+:'checkout your repository (must be done using ssh not https and 
+must be done with ssh keys from your end so no password will be 
+required. Do not submit any kind of password or ssh key as in that 
+case you will get no marks for this part of the assignment. So long 
+as you have made rmitusap a collaborator and you have setup 
+the keys correctly on your raspberry pi this is all that will be required.'
