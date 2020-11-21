500170436106f90ab105d7f316255ab6f87dba60
Peter-McAlister-s3294034 <s3294034@student.rmit.edu.au>
2020-11-21 11:31:59 +1100

    20201121 004 build_kernel

--- build_kernel.sh
+++ build_kernel.sh
@@ -14,16 +14,23 @@ COMMENT
 # There was no specification to assign a specific folder so the file will 
 # be cloned into the folder the script is run in
 
-git clone git@github.com:Peter-McAlister-s3294034/Assignment_2.git 
+git clone git@github.com:Peter-McAlister-s3294034/Assignment_2.git
 
 # This will clone the repositoryas specified from week 7
 # It wil be cloned into a sub-folder of assignment_2 called linux
 
 git clone --depth=1 https://github.com/raspberrypi/linux
 
-# Copy the .config file form the Assignment_2 file to the linux folder
+# Copy the .config file from the Assignment_2 file to the linux folder
 
-cp /Assignment_2/.config /linux
+cp -r Assignment_2/.config linux/
 
+# I used a RaspberryPi 4
+# Raspberry Pi 4 default build configuration
+# Changes directory and applies default configuration
+
+cd linux
+KERNEL=kernel7l
+make bcm2711_defconfig
 
 
